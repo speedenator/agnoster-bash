d06166724cc9d6e5cd59a0000f9dd1292db50fe0
Ant1B2x <antoine.bedex@tutanota.com>
2019-05-06 15:35:21 +0200

    added mercurial prompt

--- agnoster.bash
+++ agnoster.bash
@@ -255,6 +255,42 @@ prompt_git() {
     fi
 }
 
+# Mercurial: clean, modified and uncomitted files
+prompt_hg() {
+    local rev st branch
+    if $(hg id >/dev/null 2>&1); then
+        if $(hg prompt >/dev/null 2>&1); then
+            if [[ $(hg prompt "{status|unknown}") = "?" ]]; then
+                # if files are not added
+                prompt_segment red white
+                st='±'
+            elif [[ -n $(hg prompt "{status|modified}") ]]; then
+                # if any modification
+                prompt_segment yellow black
+                st='±'
+            else
+                # if working copy is clean
+                prompt_segment green black $CURRENT_FG
+            fi
+            PR="$PR$(hg prompt "☿ {rev}@{branch}") $st"
+        else
+            st=""
+            rev=$(hg id -n 2>/dev/null | sed 's/[^-0-9]//g')
+            branch=$(hg id -b 2>/dev/null)
+            if `hg st | grep -q "^\?"`; then
+                prompt_segment red white
+                st='±'
+            elif `hg st | grep -q "^[MA]"`; then
+                prompt_segment yellow black
+                st='±'
+            else
+                prompt_segment green black $CURRENT_FG
+            fi
+            PR="$PR☿ $rev@$branch $st"
+        fi
+    fi
+}
+
 # Dir: current working directory
 prompt_dir() {
     prompt_segment blue black '\w'
@@ -399,6 +435,7 @@ build_prompt() {
     prompt_virtualenv
     prompt_dir
     prompt_git
+    prompt_hg
     prompt_end
 }
 
